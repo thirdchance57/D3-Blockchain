b3d1ff08ede4d7f7820dcf210dd39e339c831fe8
thirdchance57 <thirdchance57@hotmail.com>
2014-12-07 18:59:37 -0800

    displaying txt  value amount priced in dollars

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var wsUri = "wss://ws.blockchain.info/inv";
-var output;
+var initOutput;
 var TYPE_BLOCK = "block";
 
 function init() {
-  // output = document.getElementById("output");
+  // initOutput = document.getElementById("output");
   initWebSocket();
 }
 
@@ -12,9 +12,10 @@ function initWebSocket() {
   var blockchain = new WebSocket('ws://ws.blockchain.info/inv');
   
 
-  //  subscribe to uncofirmed activity
+  
   blockchain.onopen = function () {
-    blockchain.send( JSON.stringify( {"op":"unconfirmed_sub"} ) );
+    blockchain.send( JSON.stringify( {"op":"unconfirmed_sub"} ) );  //  subscribe to uncofirmed activity
+    blockchain.send( JSON.stringify( {"op":"blocks_sub"} ) );   //  subscribe to new blocks
   };
  
  // when messages is received turn it to json and pass it to message.data
@@ -29,28 +30,33 @@ function initWebSocket() {
       for(var i = 0; i < response.x.out.length; i++ )
         amount += response.x.out[i].value;
       
+      // DIVIDES THE AMOUNT TO COVERT TO BTC
       response.amount = amount / 100000000;
+      initOutput = response.amount;
+      console.log(initOutput);
+
+      // PRINTS THE INITAL OUTPUT TO THE HTML 
+      document.getElementById("output").innerHTML = "$" + (initOutput.toString().match(/^\d+(?:\.\d{0,2})?/)) * 375;
 
       if( response.amount <= 3 ) {
         response.amount = 3 ;
       }
-      console.log (response.amount);
-
     }
 
-    // blocks created
+    // if a block is created
     else if( response.op == "block" ) {
-      response.type = TYPE_BLOCK;
+      console.log("BLOCK FOUND BLOCK FOUND BLOCK FOUND BLOCK FOUND");
       response.amount = Math.round( response.x.height / 10000 );
     }
     
+    // this function fires when an onmessage is received
     writeToScreen(response.amount);
   };
 }
 
-
+// fires a function to drop a d3 token (message is the btc transaction size from response.amount)
 function writeToScreen(message) {
-  var pre = barChart.addToken( {
+  barChart.addToken( {
     id:'myId',
     size: message,
     category:0,
@@ -58,8 +64,7 @@ function writeToScreen(message) {
       src:'btc.png'
     }
   });
-
-  // message = document.getElementByClass('output');
 }
 
+// fires init function when the window is loaded
 window.addEventListener("load", init, false);
