b3d81eaf31f44642fd417770d0f884b9c57c8905
thirdchance57 <thirdchance57@hotmail.com>
2014-12-09 16:05:19 -0800

    adding use strict for annotation

--- app.js
+++ app.js
@@ -8,15 +8,21 @@ function init() {
 }
 
 function initWebSocket() {
-  //  init blockchain websocket (activity, blocks)
+  "use strict";
+    //  init blockchain websocket (activity, blocks)
   var blockchain = new WebSocket('ws://ws.blockchain.info/inv');
-  
-
+  // var litecoin = new WebSocket('ws://ws.ltcchain.com:8000');
   
   blockchain.onopen = function () {
     blockchain.send( JSON.stringify( {"op":"unconfirmed_sub"} ) );  //  subscribe to uncofirmed activity
     blockchain.send( JSON.stringify( {"op":"blocks_sub"} ) );   //  subscribe to new blocks
   };
+
+  // litecoin.onopen = function () {
+  //   litecoin.send( JSON.stringify( {"op":"unconfirmed_sub"} ) );  //  subscribe to uncofirmed activity
+  //   litecoin.send( JSON.stringify( {"op":"blocks_sub"} ) );   //  subscribe to new blocks
+ 
+  // };
  
  // when messages is received turn it to json and pass it to message.data
   blockchain.onmessage = function (message) {
@@ -30,16 +36,20 @@ function initWebSocket() {
       for(var i = 0; i < response.x.out.length; i++ )
         amount += response.x.out[i].value;
       
-      // DIVIDES THE AMOUNT TO COVERT TO BTC
+      // DIVIDES THE AMOUNT and COVERTS TO BTC
       response.amount = amount / 100000000;
       initOutput = response.amount;
       console.log(initOutput);
 
-      // PRINTS THE INITAL OUTPUT TO THE HTML 
+      // PRINTS THE INITAL OUTPUT TO THE HTML        converts to string and back to integer 
       document.getElementById("output").innerHTML = "$" + (initOutput.toString().match(/^\d+(?:\.\d{0,2})?/)) * 375;
 
-      if( response.amount <= 3 ) {
-        response.amount = 3 ;
+      // these if else sets the min and max size tokens for d3
+      if( response.amount <= 5 ) {
+        response.amount = 5 ;
+      }
+      else if( response.amount >= 140   ) {
+        response.amount = 140 ;
       }
     }
 
@@ -56,12 +66,13 @@ function initWebSocket() {
 
 // fires a function to drop a d3 token (message is the btc transaction size from response.amount)
 function writeToScreen(message) {
+  "use strict";
   barChart.addToken( {
     id:'myId',
     size: message,
     category:0,
     texture: {
-      src:'btc.png'
+      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc-tI1b7luXJgfHdwbTwCmSpciFwSrVG_cqMlOeF-0CEzrS6NAxw'
     }
   });
 }
